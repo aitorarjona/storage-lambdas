99380e49cf1b5be461d5b56a5144dab4166a3414
Aitor <aitor.a98@gmail.com>
2022-04-21 18:06:20 +0200

    Format

--- modules/lidar/lidar.py
+++ modules/lidar/lidar.py
@@ -15,17 +15,17 @@ class LASModule:
         logger.debug('starting preprocessing')
         t0 = time.perf_counter()
         tmp_token = secrets.token_urlsafe(nbytes=4)
-        tmp_file_name = f"/tmp/tmpindex-{tmp_token}.lax"
+        tmp_file_name = f'/tmp/tmpindex-{tmp_token}.lax'
 
         index_proc = await asyncio.create_subprocess_shell(
-            cmd=f"bin/lasindex -stdin -o {tmp_file_name}",
+            cmd=f'bin/lasindex -stdin -o {tmp_file_name}',
             stdin=asyncio.subprocess.PIPE,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
         )
 
         compress_proc = await asyncio.create_subprocess_shell(
-            cmd="bin/laszip -stdin -stdout -olaz",
+            cmd='bin/laszip -stdin -stdout -olaz',
             stdin=asyncio.subprocess.PIPE,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
@@ -45,7 +45,7 @@ class LASModule:
 
         logger.debug('processing rest of output')
         output_chunk = await compress_proc.stdout.read(65536)
-        while output_chunk != b"":
+        while output_chunk != b'':
             await self.output_stream.write(output_chunk)
             output_chunk = await compress_proc.stdout.read(65536)
         logger.debug('all output sent')
@@ -67,7 +67,7 @@ class LASModule:
         logger.debug('puting index to redis')
         async with aiofiles.open(tmp_file_name, 'rb') as f:
             index_data = await f.read()
-            await self.redis_client.set("salou-index", index_data)
+            await self.redis_client.set('salou-index', index_data)
 
         logger.debug('preprocessing ended')
         t1 = time.perf_counter()
@@ -78,7 +78,7 @@ class LASModule:
         t0 = time.perf_counter()
 
         proc = await asyncio.create_subprocess_shell(
-            cmd=f"bin/las2las -verbose -stdin -stdout -olas -inside {min_X} {min_Y} {max_X} {max_Y}",
+            cmd=f'bin/las2las -verbose -stdin -stdout -olas -inside {min_X} {min_Y} {max_X} {max_Y}',
             stdin=asyncio.subprocess.PIPE,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
@@ -86,14 +86,14 @@ class LASModule:
 
         async def input_writer():
             input_chunk = await self.input_stream.read(65536)
-            while input_chunk != b"":
+            while input_chunk != b'':
                 proc.stdin.write(input_chunk)
                 input_chunk = await self.input_stream.read(65536)
             logger.debug('done writing input')
 
         async def output_reader():
             output_chunk = await proc.stdout.read()
-            while output_chunk != b"":
+            while output_chunk != b'':
                 await self.response.send(output_chunk)
                 output_chunk = await proc.stdout.read()
             logger.debug('done reading output')
